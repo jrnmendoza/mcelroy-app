59eb6a9a617f12a09ccded56e42734d1537cd379
John Ryan N. Mendoza <john_ryan.mendoza@med.lu.se>
2026-09-24 15:08:21 +0200

    Fix Router basename for GH Pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/join/:code" element={<ParticipantView />} />
